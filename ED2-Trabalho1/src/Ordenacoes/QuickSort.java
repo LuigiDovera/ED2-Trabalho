ff3a439ebb29d3be02a2d9f84c4d72f7ce9a9145
Matheus Levy <levybessa2001@hotmail.com>
2021-11-17 21:23:30 -0300

    att quicksort randompivo

--- ED2-Trabalho1/src/Ordenacoes/QuickSort.java
+++ ED2-Trabalho1/src/Ordenacoes/QuickSort.java
@@ -6,6 +6,7 @@ package Ordenacoes;
 
 import InsertSort.InsertSort;
 import static Ordenacoes.MergeSort.op;
+import java.util.Random;
 
 /**
  *
@@ -23,47 +24,77 @@ public class QuickSort {
         int tamanho = fim - inicio;
         if (tamanho <= 15){
             op += InsertSort.insertSort(vetor,inicio,fim,op,ordem);
-        }
-        if (inicio < fim - 1){
-            int posicaoPivo = particiona(vetor, inicio, fim-1, ordem);
-            quickSort(vetor, inicio, posicaoPivo -1, ordem);
-            quickSort(vetor, posicaoPivo+ 1 ,fim, ordem);
+        }else{
+            if (inicio < fim - 1){
+                int posicaoPivo = particiona(vetor, inicio, fim-1, ordem, true);
+                quickSort(vetor, inicio, posicaoPivo -1, ordem);
+                quickSort(vetor, posicaoPivo+ 1 ,fim, ordem);
+            }
         }
     }
     
-    private static <T extends Comparable<T>> int particiona(T[] vetor, int inicio, int fim, int ordem){
-        T pivo = vetor[inicio];
+        
+    
+    private static <T extends Comparable<T>> int particiona(T[] vetor, int inicio, int fim, int ordem, boolean randomizePivo){
+        T pivo;
+        if(randomizePivo){
+            pivo = vetor[getRandomNumberInRange(inicio,fim)];
+        }else{
+            pivo = vetor[inicio];
+        }
+        
         int i = inicio +1, f = fim;
         
         while(i <= f){
             if(ordem == 1){
-                if(vetor[i].compareTo(pivo)<= 0)
+                if(vetor[i].compareTo(pivo)<= 0){
                     i++;
-                else if (pivo.compareTo(vetor[f]) < 0)
+                    op++;
+                }
+                else if (pivo.compareTo(vetor[f]) < 0){
                     f--;
+                    op++;
+                }
                 else{
                     T troca  = vetor[i];
                     vetor[i] = vetor[f];
                     vetor[f] = troca;
                     i++;
                     f--;
+                    op++;
                 }
             }else{
-                if(vetor[i].compareTo(pivo) >= 0)
+                if(vetor[i].compareTo(pivo) >= 0){
                     i++;
-                else if (pivo.compareTo(vetor[f]) > 0)
+                    op++;
+                }
+                else if (pivo.compareTo(vetor[f]) > 0){
                     f--;
+                    op++;
+                }                    
                 else{
                     T troca  = vetor[i];
                     vetor[i] = vetor[f];
                     vetor[f] = troca;
                     i++;
                     f--;
+                    op++;
                 }
             }
         }
         vetor[inicio] = vetor[f];
         vetor[f] = pivo;
         return f;
+    }
+    
+    
+    private static int getRandomNumberInRange(int min, int max) {
+
+            if (min >= max) {
+                    throw new IllegalArgumentException("max must be greater than min");
+            }
+
+            Random r = new Random();
+            return r.nextInt((max - min) + 1) + min;
     }
 }
